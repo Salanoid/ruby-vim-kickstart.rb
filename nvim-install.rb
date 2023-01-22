4929d8d05cfc443bde4fbd0a4a61d74e474800ea
Salajan Silviu <salanoid@gmail.com>
2023-01-21 17:36:48 -0800

    Update nvim-install.rb
    add fd-find and ripgrep as dependencies needed for search in files from directory with telescope

--- nvim-install.rb
+++ nvim-install.rb
@@ -2,8 +2,12 @@
 `sudo apt purge --auto-remove neovim`
 `sudo rm -rf ~/.config/nvim`
 puts "Old neovim from packages deleted"
+
 `sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl do xygen`
+`sudo apt-get install fd-find`
+`sudo apt-get install ripgrep`
 puts "Dependencies installed"
+
 `git clone https://github.com/neovim/neovim`
 `cd neovim && git checkout stable`
 `sudo rm /usr/local/bin/nvim`
@@ -14,11 +18,14 @@ unless `$CMAKE_INSTALL_PREFIX`.nil? || `$CMAKE_INSTALL_PREFIX`.strip.empty?
 end
 `sudo rm -r build/`
 puts "Old neovim versions deleted"
+
 `cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo`
 `cd neovim && sudo make install`
 puts "Neovim installed"
+
 `mkdir ~/.config/nvim`
 `cd ~/.config/nvim; wget https://raw.githubusercontent.com/Salanoid/kickstart.nvim/master/init.lua`
 puts "Neovim configured"
+
 `sudo rm -rf neovim`
 puts "Succsesfully installed neovim."
